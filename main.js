f08673c1e0f0dd11dc0f2363a560046133805923
igneous-intrusion <40968155+igneous-intrusion@users.noreply.github.com>
2021-09-06 15:17:06 -0700

    adding Main.js
    Not fully functional..

---
+++ main.js
@@ -0,0 +1,90 @@
+// All valid credit card numbers
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5]
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
+
+// All invalid credit card numbers
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5]
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3]
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4]
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5]
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]
+
+// Can be either valid or invalid
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4]
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9]
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3]
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
+
+// An array of all the arrays above
+const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
+
+
+// Add your functions below:
+//Create a function, validateCred() that has a parameter of an array. The purpose of validateCred() is to return true when an array contains digits of a valid credit card number and false when it is invalid. This function should NOT mutate the values of the original array.
+const validateCred = (creditArr) => {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    let test = [1, 2, 3, 4].reduce(reducer)
+    let ccCopy = creditArr.slice();  //Copy of array passed
+    for (let i = ccCopy.length - 2; i >= 0; i -= 2) {
+        ccCopy[i] *= 2;
+        if (ccCopy[i] > 9) {
+            ccCopy[i] -= 9
+        }  
+    }
+    let cardSum = ccCopy.reduce(reducer) ;
+      // let cardSum = ccCopy.reduce() ;
+      
+    let result = cardSum % 10 === 0;
+    return result;
+    }
+    
+
+
+//else return false
+//console.log('hello')
+
+
+
+
+//Create another function, findInvalidCards() that has one parameter for a nested array of credit card numbers. The role of findInvalidCards() is to check through the nested array for which numbers are invalid, and return another nested array of invalid cards.
+function findInvalidCards(nestedArray){
+    let invalidNested = [];
+
+    for (let i = 0; i < nestedArray.length; i++) {
+        let currCred = nestedArray[i];
+        if (!validateCred(currCred)) {
+            invalidNested.push(currCred);
+        }
+    }
+    return invalidNested;
+}
+//Test cases
+//console.log(findInvalidCards([valid1, valid2, valid3, valid4, valid5]));// Shouldn't print anything
+//console.log(findInvalidCards([invalid1, invalid2, invalid3, invalid4, invalid5])); // Should print all of the numbers
+//console.log(findInvalidCards(batch)); // Test what the mystery numbers are
+let invalidCards = findInvalidCards(batch);
+for (let i = 0; i < invalidCards.length; i ++){
+    console.log(invalidCards[i])
+}
+console.log('end?')
+console.log(mystery1)
+
+
+
+
+// 
+
+
+
+//return newNested
+
+
+
+
+
+//idInvalidCardCompanies()
+
